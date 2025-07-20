9f5815bdb35a5223aebd506de9a9ccfc058f99f3
aaaasmirnova <smirnovnana@yandex.ru>
2025-07-20 17:05:30 +0300

    Исправлены импорты и форма логина

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './auth/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import EmployeesList from './components/EmployeesList';
-
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
     return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/" element={
+        <div className="App">
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                    path="/"
+                    element={
                         <ProtectedRoute>
                             <EmployeesList />
                         </ProtectedRoute>
-                    } />
-                </Routes>
-            </Router>
-        </AuthProvider>
+                    }
+                />
+            </Routes>
+        </div>
     );
 }
 
-export default App; // Эта строка обязательна!
+export default App;
